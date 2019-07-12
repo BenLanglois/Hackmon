dc94c01c6a93a87b59cd39e66b5da27ef1554da8
Ariel Lam <ariel.lam8@gmail.com>
2019-07-12 15:21:49 -0400

    Add getters and setters to Stats class

--- game/stats.h
+++ game/stats.h
@@ -1,8 +1,9 @@
 #ifndef STATS_H
 #define STATS_H
 
+#include "statInfo.h"
+
 class Stats {
-  public:
     const int maxHp;
     const int baseAttack;
     const int baseDefense;
@@ -11,10 +12,38 @@ class Stats {
     int attack;
     int defense;
     int speed;
-
-    Stat(const int mapHp, const int baseAttack, const int baseDefense, const int baseSpeed, int hp, int attack, int defense, int speed):
+  public:
+    Stats(const int mapHp, const int baseAttack, const int baseDefense, const int baseSpeed, int hp, int attack, int defense, int speed):
       maxHp{maxHp}, baseAttack{baseAttack}, baseDefense{baseDefense}, baseSpeed{baseSpeed}, hp{hp}, attack{attack},
       defense{defense}, speed{defense};
+    const int getStat(StatName s) {
+      switch(s) {
+        case HP:
+          return hp;
+        case ATTACK:
+          return attack;
+        case DEFENSE:
+          return defense;
+        case SPEED:
+          return speed;
+      }
+    }
+    void setStat(StatName s, int newValue) {
+      switch(s) {
+        case HP:
+          hp = newValue;
+          break;
+        case ATTACK:
+          attack = newValue;
+          break;
+        case DEFENSE:
+          defense = newValue;
+          break;
+        case SPEED:
+          speed = newValue;
+          break;
+      }
+    }
 };
 
 #endif
