ff2b258c390ed6c0ac5628d5a469f53cf6ec7249
Alexandra Girard <alexgirard999@gmail.com>
2019-07-13 18:54:57 -0400

    changing to unique pointers

--- game/species.cc
+++ game/species.cc
@@ -4,7 +4,7 @@
 #include "species.h"
 using namespace std;
 
-Species::Species(const string speciesName, vector<Move*> moves, Move specialMove, Stats stats, vector<Family> family):
+Species::Species(const string speciesName, std::vector<unique_ptr<Move>> moves, Move specialMove, Stats stats, vector<Family> family):
   speciesName{speciesName}, moves{moves}, specialMove{specialMove}, stats{stats}, family{family} {}
 
 Species::Hackmon createHackmon(vector<Move*> moves, const string hackmonName = string()) {
