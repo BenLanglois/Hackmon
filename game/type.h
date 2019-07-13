612ea2e967be5e46daa03ae60e99ee25dd281129
Ariel Lam <ariel.lam8@gmail.com>
2019-07-13 16:43:46 -0400

    Modify effectiveness values

--- game/type.h
+++ game/type.h
@@ -17,22 +17,22 @@ enum Type {
 
 
 const std::map<Type, std::map<Type,size_t>> typeChart = {
-  {NORMAL, std::map<Type,size_t>{{NORMAL, 100}, {FIRE, 100}, {WATER, 100}, {ELECTRIC, 100},
-            {GRASS, 100}, {FIGHTING, 100}, {GROUND, 100}, {PSYCHIC, 100}}},
-  {FIRE, std::map<Type,size_t>{{NORMAL, 100}, {FIRE, 50}, {WATER, 50}, {ELECTRIC, 100},
-            {GRASS, 200}, {FIGHTING, 100}, {GROUND, 100}, {PSYCHIC, 100}}},
-  {WATER, std::map<Type,size_t>{{NORMAL, 100}, {FIRE, 200}, {WATER, 50}, {ELECTRIC, 100},
-            {GRASS, 50}, {FIGHTING, 100}, {GROUND, 200}, {PSYCHIC, 100}}},
-  {ELECTRIC, std::map<Type,size_t>{{NORMAL, 100}, {FIRE, 100}, {WATER, 200}, {ELECTRIC, 50},
-            {GRASS, 50}, {FIGHTING, 100}, {GROUND, 0}, {PSYCHIC, 100}}},
-  {GRASS, std::map<Type,size_t>{{NORMAL, 100}, {FIRE, 50}, {WATER, 200}, {ELECTRIC, 100},
-            {GRASS, 50}, {FIGHTING, 100}, {GROUND, 200}, {PSYCHIC, 100}}},
-  {FIGHTING, std::map<Type,size_t>{{NORMAL, 200}, {FIRE, 100}, {WATER, 100}, {ELECTRIC, 100},
-            {GRASS, 100}, {FIGHTING, 100}, {GROUND, 100}, {PSYCHIC, 50}}},
-  {GROUND, std::map<Type,size_t>{{NORMAL, 100}, {FIRE, 200}, {WATER, 100}, {ELECTRIC, 200},
-            {GRASS, 50}, {FIGHTING, 100}, {GROUND, 100}, {PSYCHIC, 100}}},
-  {PSYCHIC, std::map<Type,size_t>{{NORMAL, 100}, {FIRE, 100}, {WATER, 100}, {ELECTRIC, 100},
-            {GRASS, 100}, {FIGHTING, 200}, {GROUND, 100}, {PSYCHIC, 50}}}
+  {NORMAL, std::map<Type,size_t>{{NORMAL, 2}, {FIRE, 2}, {WATER, 2}, {ELECTRIC, 2},
+            {GRASS, 2}, {FIGHTING, 2}, {GROUND, 2}, {PSYCHIC, 2}}},
+  {FIRE, std::map<Type,size_t>{{NORMAL, 2}, {FIRE, 1}, {WATER, 1}, {ELECTRIC, 2},
+            {GRASS, 4}, {FIGHTING, 2}, {GROUND, 2}, {PSYCHIC, 2}}},
+  {WATER, std::map<Type,size_t>{{NORMAL, 2}, {FIRE, 4}, {WATER, 1}, {ELECTRIC, 2},
+            {GRASS, 1}, {FIGHTING, 2}, {GROUND, 4}, {PSYCHIC, 2}}},
+  {ELECTRIC, std::map<Type,size_t>{{NORMAL, 2}, {FIRE, 2}, {WATER, 4}, {ELECTRIC, 1},
+            {GRASS, 1}, {FIGHTING, 2}, {GROUND, 0}, {PSYCHIC, 2}}},
+  {GRASS, std::map<Type,size_t>{{NORMAL, 2}, {FIRE, 1}, {WATER, 4}, {ELECTRIC, 2},
+            {GRASS, 1}, {FIGHTING, 2}, {GROUND, 4}, {PSYCHIC, 2}}},
+  {FIGHTING, std::map<Type,size_t>{{NORMAL, 4}, {FIRE, 2}, {WATER, 2}, {ELECTRIC, 2},
+            {GRASS, 2}, {FIGHTING, 2}, {GROUND, 2}, {PSYCHIC, 1}}},
+  {GROUND, std::map<Type,size_t>{{NORMAL, 2}, {FIRE, 4}, {WATER, 2}, {ELECTRIC, 4},
+            {GRASS, 1}, {FIGHTING, 2}, {GROUND, 2}, {PSYCHIC, 2}}},
+  {PSYCHIC, std::map<Type,size_t>{{NORMAL, 2}, {FIRE, 2}, {WATER, 2}, {ELECTRIC, 2},
+            {GRASS, 2}, {FIGHTING, 4}, {GROUND, 2}, {PSYCHIC, 1}}}
 };
 
 #endif
