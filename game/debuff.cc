3573f3cfb44418d38cb1da6ead085a8de5f22f61
Ariel Lam <ariel.lam8@gmail.com>
2019-07-13 22:52:04 -0400

    Change debuff ctor and add debuffMap

--- game/debuff.cc
+++ game/debuff.cc
@@ -1,5 +1,6 @@
 #include "debuff.h"
 
-Debuff::Debuff(const DebuffName name, const StatName stat, const int strength) : 
-                name{name}, stat{stat}, strength{strength} {};
+Debuff::Debuff(const DebuffName name) : name{name}, stat{debuffMap.at(name)} {
+
+}
 
--- game/debuffInfo.h
+++ game/debuffInfo.h
@@ -1,11 +1,22 @@
 #ifndef DEBUFFINFO_H_
 #define DEBUFFINFO_H_
 
+#include "statInfo.h"
+#include <map>
+#include <utility>
+
 enum DebuffName {
   HEALTHY,
   POISONED,
   BURNED,
   FROZEN
 };
+
+std::map<DebuffName, StatName> debuffMap = {
+  {HEALTHY, HP},
+  {POISONED, ATTACK},
+  {BURNED, DEFENSE},
+  {FROZEN, SPEED}
+}
 
 #endif
