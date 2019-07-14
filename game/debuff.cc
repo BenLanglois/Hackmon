38aa7b3b137d23e9808608a8b256449b4a45d2cb
Ariel Lam <ariel.lam8@gmail.com>
2019-07-13 22:53:12 -0400

    Fix debuff.cc

--- game/debuff.cc
+++ game/debuff.cc
@@ -1,6 +1,5 @@
 #include "debuff.h"
 
-Debuff::Debuff(const DebuffName name) : name{name}, stat{debuffMap.at(name)} {
-
-}
+Debuff::Debuff(const DebuffName name) : name{name}, 
+          stat{debuffMap.at(name)}, strength{name == HP ? 0 : defaultStrength} {}
 
