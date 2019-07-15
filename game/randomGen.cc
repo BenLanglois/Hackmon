93c3ac1f9e65e324ca038d1573901fcfc51442b0
Ariel Lam <ariel.lam8@gmail.com>
2019-07-14 21:20:46 -0400

    Fix randGen MIL

--- game/randomGen.cc
+++ game/randomGen.cc
@@ -6,8 +6,9 @@ using namespace std;
 random_device RandomGenerator::rd {};
 mt19937 RandomGenerator::random_generator {rd()};
 
-RandomGenerator::RandomGenerator(const unsigned int min_, const unsigned int max_) : min{min_}, max{max_} {
-  random_distribution = uniform_int_distribution<unsigned int> {min, max};
+RandomGenerator::RandomGenerator(const unsigned int min_, const unsigned int max_) : min{min_}, max{max_},
+  random_distribution{uniform_int_distribution<unsigned int> {min, max}} {
+
 }
 
 int RandomGenerator::getRandom() {
