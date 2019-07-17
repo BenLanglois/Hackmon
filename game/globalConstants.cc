8006d960b3f5cd566ffecd24425b423a063b5e09
Ben Langlois <bdlanglo@edu.uwaterloo.ca>
2019-07-17 02:58:04 -0400

    Create globalConstants.cc

---
+++ game/globalConstants.cc
@@ -0,0 +1,5 @@
+#include "globalConstants.h"
+
+unsigned numberBattling;
+int numberParty = 6;
+int numberOfItems = 3;
--- game/globalConstants.h
+++ game/globalConstants.h
@@ -1,8 +1,8 @@
 #ifndef GLOBAL_CONSTANTS_H_
 #define GLOBAL_CONSTANTS_H_
 
-unsigned numberBattling;
-int numberParty = 6;
-int numberOfItems = 3;
+extern unsigned numberBattling;
+extern int numberParty;
+extern int numberOfItems;
 
 #endif
